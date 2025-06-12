2678bb30a2a0e1f737eee7c59061af3338a130af
Nihal Rahman <rahmannihal72@gmail.com>
2025-06-12 11:53:02 +0530

    Redundant Component import removed

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
 import { ThemeProvider } from "@/components/theme-provider";
-import { ThemeToggle } from "@/components/theme-toggle";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
